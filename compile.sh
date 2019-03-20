3562ce5aece35f6b711561a79870dab80c42f55a
Leandre <leandre.blanchard@epitech.eu>
2019-03-20 11:06:42 +0100

    cross plateform compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p build; cd build && cmake -DCMAKE_BUILD_TYPE=$1 .. && cmake --build . -- -j 4
+mkdir -p build; cd build
+if [[ "$OSTYPE" == "msys" ]]
+then
+    cmake -G "Visual Studio 15 2017 Win64" .. -Wno-dev && cmake --build . --config Release
+else
+    cmake -DCMAKE_BUILD_TYPE=$1 .. && cmake --build . -- -j 4
+fi
+    
